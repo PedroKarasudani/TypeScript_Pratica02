de2c36b0c90b83f97fe4e889e6f28221048d83c7
Pedro <pedro@eletrocenterlondrina.com.br>
2024-08-30 17:24:22 -0300

    Prev e Next

--- src/Slide.ts
+++ src/Slide.ts
@@ -5,6 +5,7 @@ export default class Slide {
   time: number;
   index: number
   slide: Element;
+  
   constructor(
     container: Element,
     slides: Element[],
@@ -17,7 +18,7 @@ export default class Slide {
     this.time = time;
     this.index = 0;
     this.slide = this.slides[this.index]
-    this.show(this.index)
+    this.init();
   }
 
   hide(el: Element) {
@@ -30,5 +31,30 @@ export default class Slide {
 
     this.slides.forEach(el => this.hide(el));
     this.slide.classList.add('active');
+  }
+
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev)
+  }
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next)
+  }
+
+  private addControls() {
+    const prevButton = document.createElement('button');
+    const nextButton = document.createElement('button');
+    prevButton.innerText = "Slide Anterior";
+    nextButton.innerText = "Proximo Slide";
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+    prevButton.addEventListener('pointerup', () => this.prev());
+    nextButton.addEventListener('pointerup', () => this.next());
+    
+  }
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }

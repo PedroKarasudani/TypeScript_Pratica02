92e4a7a83914785737875667d4634eb3f6b7c677
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-08-31 17:39:13 -0300

    Auto slide

--- src/Slide.ts
+++ src/Slide.ts
@@ -1,11 +1,14 @@
+import Timeout from './Timeout.js';
+
 export default class Slide {
   container: Element;
   slides: Element[];
   controls: Element;
   time: number;
-  index: number
+  index: number;
   slide: Element;
-  
+  timeout: Timeout | null;
+
   constructor(
     container: Element,
     slides: Element[],
@@ -17,41 +20,47 @@ export default class Slide {
     this.controls = controls;
     this.time = time;
     this.index = 0;
-    this.slide = this.slides[this.index]
+    this.slide = this.slides[this.index];
     this.init();
+    this.timeout = null;
   }
 
   hide(el: Element) {
-    el.classList.remove('active')
+    el.classList.remove('active');
   }
 
-  show(index: number){
+  show(index: number) {
     this.index = index;
     this.slide = this.slides[this.index];
 
-    this.slides.forEach(el => this.hide(el));
+    this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add('active');
+    this.auto(this.time);
+  }
+
+  auto(time: number) {
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
   }
 
   prev() {
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
-    this.show(prev)
+    this.show(prev);
   }
   next() {
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
-    this.show(next)
+    this.show(next);
   }
 
   private addControls() {
     const prevButton = document.createElement('button');
     const nextButton = document.createElement('button');
-    prevButton.innerText = "Slide Anterior";
-    nextButton.innerText = "Proximo Slide";
+    prevButton.innerText = 'Slide Anterior';
+    nextButton.innerText = 'Proximo Slide';
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
     prevButton.addEventListener('pointerup', () => this.prev());
     nextButton.addEventListener('pointerup', () => this.next());
-    
   }
   private init() {
     this.addControls();
